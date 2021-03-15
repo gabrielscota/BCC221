63164a7ac66dc78d2739dc833cce8c41ee082292
v_cossio <vinicossio@gmail.com>
2021-03-15 01:00:38 -0300

    feat: Time_Sheet.cpp done.

---
+++ TP1 - C++/data/models/receptionist.cpp
@@ -0,0 +1,2 @@
+#include "receptionist.hpp"
+
---
+++ TP1 - C++/data/models/time_sheet.cpp
@@ -0,0 +1,20 @@
+#include "time_sheet.hpp"
+
+TimeSheet::TimeSheet(string id, vector<Point *> points):
+    id(id),
+    points(points){};
+
+TimeSheet::~TimeSheet(){}
+
+string TimeSheet::getId(){
+    return this->id;
+}
+
+vector <Point *> TimeSheet::getPoints(){
+    return this->points;
+}
+
+void TimeSheet::setPoints(vector<Point *> points){
+    this->points = points;
+}
+
