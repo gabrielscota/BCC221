66e87fde0e53ab9207a4fe71bcc253d75bc1f8a9
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 20:47:57 -0300

    feat: added Person entitie

---
+++ TP2 - Java/src/domain/entities/Person.java
@@ -0,0 +1,23 @@
+package domain.entities;
+
+public class Person {
+    private String id;
+    private String name;
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
