a14e014f44d0c75a586846d79f41e831d694ab96
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 20:52:11 -0300

    feat: added Consultation entitie

---
+++ TP2 - Java/src/domain/entities/Consultation.java
@@ -0,0 +1,53 @@
+package domain.entities;
+
+public class Consultation {
+    private String id;
+    private Patient patient;
+    private String date;
+    private String description;
+    private PaymentConsultation paymentConsultation;
+
+    public Consultation(String id, Patient patient, String date, String description, PaymentConsultation paymentConsultation) {
+        this.id = id;
+        this.patient = patient;
+        this.date = date;
+        this.description = description;
+        this.paymentConsultation = paymentConsultation;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public PaymentConsultation getPaymentConsultation() {
+        return paymentConsultation;
+    }
+
+    public void setPaymentConsultation(PaymentConsultation paymentConsultation) {
+        this.paymentConsultation = paymentConsultation;
+    }
+}
