b184bf08c6fddd188e8c3326b5781542b8782e73
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 20:56:26 -0300

    feat: added Payment entitie

---
+++ TP2 - Java/src/domain/entities/Payment.java
@@ -0,0 +1,43 @@
+package domain.entities;
+
+public class Payment {
+    private String id;
+    private String date;
+    private double value;
+    private String description;
+
+    public Payment(String id, String date, double value, String description) {
+        this.id = id;
+        this.date = date;
+        this.value = value;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
