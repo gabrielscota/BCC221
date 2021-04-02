51c1291de8000e9e009ac7977016f3346eef8d7d
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 23:17:37 -0300

    feat: added LoginPage in ui layer

--- TP2 - Java/src/Main.java
+++ TP2 - Java/src/Main.java
@@ -1,21 +1,17 @@
+import data.usecases.authentication.LocalAuthentication;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import presentation.controllers.PresentationLoginController;
+import ui.pages.login.LoginPage;
 
 public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ui/pages/login/login.fxml"));
-        primaryStage.setTitle("Clínica Odontológica");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
-        primaryStage.show();
+    public static void main(String[] args){
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        LoginPage loginPage = new LoginPage(new PresentationLoginController(new LocalAuthentication()));
+        loginPage.build(primaryStage);
     }
 }
---
+++ TP2 - Java/src/ui/pages/login/LoginPage.java
@@ -0,0 +1,22 @@
+package ui.pages.login;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginPage {
+    private final LoginController loginController;
+
+    public LoginPage(LoginController loginController) {
+        this.loginController = loginController;
+    }
+
+    public void build(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("Login | Clínica Odontológica");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
