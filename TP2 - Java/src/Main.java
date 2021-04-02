823b8292ab86f5357c076665075e2de067536c3a
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 22:16:57 -0300

    feat: added login page ui

--- TP2 - Java/src/Main.java
+++ TP2 - Java/src/Main.java
@@ -8,9 +8,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ui/pages/home/home.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("ui/pages/login/login.fxml"));
+        primaryStage.setTitle("Clínica Odontológica");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
