466d224b2d5c94adab0754bae3865dff279127ad
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 21:00:30 -0300

    feat: added Expense entitie

---
+++ TP2 - Java/src/domain/entities/Expense.java
@@ -0,0 +1,63 @@
+package domain.entities;
+
+public class Expense {
+    private String id;
+    private String description;
+    private String dueDate;
+    private String paymentDate;
+    private String expenseType;
+    private float value;
+
+    public Expense(String id, String description, String dueDate, String paymentDate, String expenseType, float value) {
+        this.id = id;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.paymentDate = paymentDate;
+        this.expenseType = expenseType;
+        this.value = value;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(String paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public String getExpenseType() {
+        return expenseType;
+    }
+
+    public void setExpenseType(String expenseType) {
+        this.expenseType = expenseType;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+}
