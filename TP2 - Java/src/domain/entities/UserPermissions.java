2b76e43a9cc772c5533926b43999af670f4e5aa8
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 21:04:26 -0300

    feat: added UserPermissions entitie

---
+++ TP2 - Java/src/domain/entities/UserPermissions.java
@@ -0,0 +1,45 @@
+package domain.entities;
+
+public class UserPermissions {
+    private String id;
+    private String permissions[];
+
+    public UserPermissions(String id, String[] permissions) {
+        this.id = id;
+        this.permissions = permissions;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String[] getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String[] permissions) {
+        this.permissions = permissions;
+    }
+}
+
+class GeneralUser extends UserPermissions {
+    public GeneralUser(String id, String[] permissions) {
+        super(id, permissions);
+    }
+}
+
+class AdministrativeAssistantUser extends UserPermissions {
+    public AdministrativeAssistantUser(String id, String[] permissions) {
+        super(id, permissions);
+    }
+}
+
+class AdministrativeUser extends UserPermissions {
+    public AdministrativeUser(String id, String[] permissions) {
+        super(id, permissions);
+    }
+}
