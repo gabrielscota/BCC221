59fab633383f42ab1fb32071b99c6df8c59db689
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 21:42:59 -0300

    feat: added authentication domain and data usecase

---
+++ TP2 - Java/src/data/usecases/authentication/LocalAuthentication.java
@@ -0,0 +1,11 @@
+package data.usecases.authentication;
+
+import domain.usecases.authentication.AuthenticationParams;
+import domain.usecases.authentication.UserAuthentication;
+
+public class LocalAuthentication implements UserAuthentication {
+    @Override
+    public String auth(AuthenticationParams params) {
+        return params.getLogin() == "admin" && params.getPassword() == "123456" ? "Admin" : null;
+    }
+}
---
+++ TP2 - Java/src/domain/usecases/authentication/UserAuthentication.java
@@ -0,0 +1,6 @@
+package domain.usecases.authentication;
+
+public interface UserAuthentication {
+    String auth(AuthenticationParams params);
+}
+
