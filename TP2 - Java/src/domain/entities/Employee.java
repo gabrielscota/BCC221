3fae365840ac3698d072a0b93734b42c1eb314df
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 21:14:54 -0300

    feat: added Employee entitie

---
+++ TP2 - Java/src/domain/entities/Employee.java
@@ -0,0 +1,31 @@
+package domain.entities;
+
+public class Employee extends User {
+    private String id;
+    private TimeSheet timeSheet;
+    private Payment payments[];
+
+    public Employee(String id, String login, String password, UserPermissions userPermissions) {
+        super(id, login, password, userPermissions);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public TimeSheet getTimeSheet() {
+        return timeSheet;
+    }
+
+    public void setTimeSheet(TimeSheet timeSheet) {
+        this.timeSheet = timeSheet;
+    }
+
+    public Payment[] getPayments() {
+        return payments;
+    }
+
+    public void setPayments(Payment[] payments) {
+        this.payments = payments;
+    }
+}
