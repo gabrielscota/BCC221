167e0b284270bdeb4331030caaf052d0664e8259
Gabriel Scotá Arruda <gabrielscota2015@gmail.com>
2021-04-01 21:21:42 -0300

    feat: added Orthodontis entitie

---
+++ TP2 - Java/src/domain/entities/Orthodontist.java
@@ -0,0 +1,10 @@
+package domain.entities;
+
+public class Orthodontist extends Employee {
+    private Schedule schedule;
+    private Assistent assistent;
+
+    public Orthodontist(String id, String login, String password, UserPermissions userPermissions) {
+        super(id, login, password, userPermissions);
+    }
+}
