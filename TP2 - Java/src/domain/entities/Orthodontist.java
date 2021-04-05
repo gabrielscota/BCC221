0ce59625621f2fbea29bf25b52746be9bda83f58
v_cossio <vinicossio@gmail.com>
2021-04-04 21:09:51 -0300

    Feat: Orthodontist.java done

--- TP2 - Java/src/domain/entities/Orthodontist.java
+++ TP2 - Java/src/domain/entities/Orthodontist.java
@@ -7,4 +7,19 @@ public class Orthodontist extends Employee {
     public Orthodontist(String id, String login, String password, UserPermissions userPermissions) {
         super(id, login, password, userPermissions);
     }
+    public Schedule getSchedule(){
+        return schedule;
+    }
+    public Assistent getAssistent(){
+        return assistent;
+    }
+
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+
+    public void setAssistent(Assistent assistent) {
+        this.assistent = assistent;
+    }
+
 }
